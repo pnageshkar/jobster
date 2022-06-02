2f4c28b3aceda4cc9253e77581da63bf35b04197
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2022-06-02 18:27:44 +0530

    Basic Framework done

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+
+import FormRow from "./FormRow";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
+
+
+export {Logo,FormRow,Navbar,SmallSidebar,BigSidebar};
